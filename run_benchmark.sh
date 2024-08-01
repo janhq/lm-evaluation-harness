58b0b0605f8f6a8f0d71dde7901d67f9a1759d6e
bachvudinh <bachvudinh02@gmail.com>
2024-08-01 12:29:18 +0000

    debug a small errors

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -5,6 +5,15 @@ lm_eval --model vllm \
     --model_args pretrained=$MODEL_NAME,tensor_parallel_size=$GPUS_PER_MODEL,dtype=auto,gpu_memory_utilization=0.5,data_parallel_size=$MODEL_REPLICAS,max_model_len=4096 \
     --tasks mmlu \
     --batch_size auto \
-    --output_path ./benchmark_results.json
-# display the results
-cat ./benchmark_results.json
+    --output_path ./benchmark_results/
+# find the json file
+OUTPUT_FILE=$(find ./benchmark_results/ -type f -name '*.json')
+
+# Check if the JSON file was found
+if [ -z "$OUTPUT_FILE" ]; then
+  echo "No JSON file found in the output path."
+  exit 1
+fi
+
+# display
+cat "$OUTPUT_FILE"
