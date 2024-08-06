86c151deb1a93420583cfee95f6bbc08327f201c
bachvudinh <bachvudinh02@gmail.com>
2024-08-07 04:06:31 +0700

    change to 2 gpu

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,8 +1,7 @@
 MODEL_NAME=$1
-GPUS_PER_MODEL=4
+GPUS_PER_MODEL=2
 MODEL_REPLICAS=1
-lm_eval --model vllm \         
-    --model_args pretrained=$MODEL_NAME,tensor_parallel_size=$GPUS_PER_MODEL,dtype=auto,gpu_memory_utilization=0.5,data_parallel_size=$MODEL_REPLICAS,max_model_len=4096 \
+lm_eval --model vllm --model_args pretrained=$MODEL_NAME,tensor_parallel_size=$GPUS_PER_MODEL,dtype=auto,gpu_memory_utilization=0.5,data_parallel_size=$MODEL_REPLICAS,max_model_len=4096 \
     --tasks mmlu \
     --batch_size auto \
     --output_path ./benchmark_results/
