bb5da65ba11ed26476b5f1d281823d2f643ec69b
bachvudinh <bachvudinh02@gmail.com>
2024-08-11 04:12:29 +0700

    change to use 1 gpu only ude to bug from vllm

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p ./benchmark_results
 
 MODEL_NAME="$1"
-GPUS_PER_MODEL=2
+GPUS_PER_MODEL=1
 MODEL_REPLICAS=1
 lm_eval --model vllm --model_args pretrained=$MODEL_NAME,tensor_parallel_size=$GPUS_PER_MODEL,dtype=auto,gpu_memory_utilization=0.5,data_parallel_size=$MODEL_REPLICAS,max_model_len=4096 \
     --tasks mmlu \
